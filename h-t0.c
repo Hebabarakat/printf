a1ae6d3c520e14b3c0a8871721e56db9ba585cdf
Sarahgad <sarahgad91@gmail.com>
2023-05-28 09:14:53 -0700

    complete the code

--- h-t0.c
+++ h-t0.c
@@ -12,10 +12,18 @@ int _printf(const char *format, ...)
   unsigned int a ;
   va_list arguments;
   int (*function) (va_list, char*, unsigned int);
+char c;
+char *s;
 
   va_start(argument, format);
   for (a = 0; format[a] != '\0'; a++)
     {
       if (format[a] == '%')
 	{
-	  if ( format [a+1]
+	  if ( format [a+1] == 'c')
+	c = va_arg(argument, char);
+	  else if (format[a+1] == 's')
+	s = va_ arg(argument, char*);
+	}
+	va_arg(argument, char);	
+	}
