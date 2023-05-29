29ec9837c64d0095257fb25cd52150472d51e2a3
Sarahgad <sarahgad91@gmail.com>
2023-05-29 12:24:15 -0700

    Header file update

--- main.h
+++ main.h
@@ -1,4 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include"main.h"
+#include<stdarg.h>
+#include<stdio.h>
+#include<string.h>
+#include <unistd.h>
 int _printf(const char *format, ...);
+int _printchar(char x);
+
+typedef struct conv_spec {
+	char spec;
+	int (*fuction)(valist arg);
+} conv;
+
 #endif/*main.h*/
